d9779420b0b2e8edb403bc30e1533c1152d54699
Kang MinSeok <stillnessfate@gmail.com>
2018-04-01 18:19:09 +0900

    Revert "Fixed flicker and delete useless files (issue 159)"

--- gameFunctions.py
+++ gameFunctions.py
@@ -7,7 +7,6 @@ from alien import Alien
 pauseBtnState = 1
 back = False
 x = 0
-clock = pg.time.Clock()
 FPS = 120
 def checkEvents(setting, screen, stats, sb, playBtn, quitBtn, sel, ship, aliens, bullets, eBullets):
 	"""Respond to keypresses and mouse events."""
@@ -245,7 +244,8 @@ def updateScreen(setting, screen, stats, sb, ship, aliens, bullets, eBullets, pl
 	#Redraw the screen during each pass through the loop
 	#Fill the screen with background color
 	#Readjust the quit menu btn position
-	global x, clock, FPS
+	CLOCK = pg.time.Clock()
+	global x, FPS
 	quitBtn.rect.y = 300
 	quitBtn.msgImageRect.y = 300
 	menuBtn.rect.y = 250
@@ -256,6 +256,8 @@ def updateScreen(setting, screen, stats, sb, ship, aliens, bullets, eBullets, pl
 	if rel_x < setting.screenHeight:
 		screen.blit(setting.bg, (0,rel_x))
 	x -= 1			
+	pg.display.update()
+	CLOCK.tick(FPS)
 
 	#draw all the bullets
 	for bullet in bullets.sprites():
@@ -277,7 +279,5 @@ def updateScreen(setting, screen, stats, sb, ship, aliens, bullets, eBullets, pl
 		menuBtn.drawBtn()
 		quitBtn.drawBtn()
 		sel.blitme()
-		
 	#Make the most recently drawn screen visable.
-	pg.display.update()
-	clock.tick(FPS)
+	pg.display.flip()
