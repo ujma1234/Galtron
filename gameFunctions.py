aa7e1ae45803a05135e9d1121ff315d51765cb43
Taewan Park <swimingtw@gmail.com>
2018-04-09 23:45:30 +0900

    Ship invincibility issue fix (#338)
    남은 ship의 갯수가 0일때, Invincibility 가 제대로 작동하지 않는걸 수정

--- gameFunctions.py
+++ gameFunctions.py
@@ -270,7 +270,6 @@ def changeFleetDir(setting, aliens):
 def shipHit(setting, stats, sb, screen, ship, aliens, bullets, eBullets):
     """Respond to ship being hit"""
     if stats.shipsLeft > 0:
-
         if pg.time.get_ticks() - setting.newStartTime > setting.invincibileTime:
             sounds.explosion_sound.play()
             stats.shipsLeft -= 1
@@ -279,6 +278,15 @@ def shipHit(setting, stats, sb, screen, ship, aliens, bullets, eBullets):
             ship.chargeGaugeStartTime = pg.time.get_ticks()
             # ship.centerShip()
             setting.newStartTime = pg.time.get_ticks()
+    elif stats.shipsLeft == 0:
+        if pg.time.get_ticks() - setting.newStartTime > setting.invincibileTime:
+            sounds.explosion_sound.play()
+            stats.ultimateGauge = 0
+            ship.chargeGauge = 0
+            ship.chargeGaugeStartTime = pg.time.get_ticks()
+            setting.newStartTime = pg.time.get_ticks()
+            stats.gameActive = False
+            checkHighScore(stats, sb)
     else:
         stats.gameActive = False
         checkHighScore(stats, sb)
@@ -520,7 +528,7 @@ def updateScreen(setting, screen, stats, sb, ship, aliens, bullets, eBullets, ch
     for i in items:
         i.update()
         i.drawitem()
-    #Dodge if ship is invincibile
+    #Shield if ship is invincibile
     updateInvincibility(setting, screen, ship)
 
     # Update Ultimate Gauge
